429dce7bae8a0bb5bfd69ca7266239babaf5695f
k0d3r1s <k0d3r1s@gmail.com>
2022-04-28 21:11:39 +0300

    move common cache classes to separate library

--- Adapter/PhpRedis.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Vairogs\Cache\Adapter;
-
-final class PhpRedis extends AbstractRedisAdapter
-{
-    protected string $class = self::class;
-    protected array $packages = ['ext-redis'];
-}
--- Adapter/Predis.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Vairogs\Cache\Adapter;
-
-final class Predis extends AbstractRedisAdapter
-{
-    protected string $class = self::class;
-    protected array $packages = ['predis/predis'];
-}
